f609ffb1d646c5fa3d03c1041a20c35cca4ff3cf
BhavikChhabria <bhavikchhabria@gmail.com>
2025-04-28 16:56:34 +0530

    file updated

--- obj/Debug/net8.0/middleware_demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/middleware_demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("middleware_demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413175207f8b4666616d12bf9869523700186fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9592fb28c91126a5dba12b896cde7a428663d241")]
 [assembly: System.Reflection.AssemblyProductAttribute("middleware_demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("middleware_demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
